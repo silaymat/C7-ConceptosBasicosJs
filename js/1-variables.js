3a17a5e47e7f31a230c3ad5dacb581e204152a92
silvia Aymat <silaymat@yahoo.com.ar>
2022-07-03 00:31:29 -0300

    Agregamos el código para mostrar diferentes mensajes

---
+++ js/1-variables.js
@@ -0,0 +1,22 @@
+// Esto es un comentario de una sóla línea
+
+/*Esto es un comentario
+de múltiples
+líneas*/
+
+//mostrar un mensaje en el documento html
+document.write("<h1>Hola mundo</h1>");
+
+//mostrar un mensaje en la consola
+console.log("Nuevo mensaje");
+
+//mostrar un mensaje en una ventana emergente
+//alert("Prueba de mensaje en ventana emergente");
+
+//mostrar una suma 29+6
+document.write("Resultado: " +(29+6));
+//Resultado: 35
+
+
+
+
