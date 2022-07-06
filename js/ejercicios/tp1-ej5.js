8fedb36c6725ac3180b4533d8c16072b0d0e3b31
silvia Aymat <silaymat@yahoo.com.ar>
2022-07-05 23:50:48 -0300

    Agregamos el ejercicio numero 5 del tp1

---
+++ js/ejercicios/tp1-ej5.js
@@ -0,0 +1,9 @@
+let numero1 = parseInt(prompt("Ingrese un primer número"));
+let numero2 = parseInt(prompt("Ingrese un segundo número"));
+console.log(numero1);
+console.log(numero2);
+console.log(50);
+//console.log(parseInt(numero1));
+document.write("La suma es " + (numero1+numero2));
+
+// Investigar Number
